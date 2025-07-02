07ad7c7fab6f1fbf3a9bda2fdfd85af6d79e4fd8
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2025-07-02 18:57:45 +0300

    Update fydpi_worker.sh

--- fydpi_worker.sh
+++ fydpi_worker.sh
@@ -10,7 +10,7 @@ for TARGET_UID in $(dumpsys package $1 | grep uid | cut -d= -f2 | cut -d" " -f1 
   # Report start.
   echo "[FuckYouDPI] Enabling for $1 (UID ${TARGET_UID})"
   
-  # Route.
+  # Route app to this entire fuckery.
   iptables -t mangle -A OUTPUT -p tcp -m owner --uid-owner "$TARGET_UID" -j MARK --set-mark 1
   iptables -t mangle -A PREROUTING -p tcp -m mark --mark 1 -j TPROXY --on-port "$PORT" --tproxy-mark 1
   

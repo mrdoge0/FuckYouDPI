73096ac3e736277c00addaae84a1c5645dde840a
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2025-07-02 22:26:05 +0300

    Add companion installing

--- customize.sh
+++ customize.sh
@@ -4,7 +4,7 @@
 MINAPI=27
 MAXAPI=36
 
-# Permissions
+# Permissions and companion install.
 set_permissions() {
   set_perm_recursive $MODPATH/static-aarch64      0 0 0755 0755
   set_perm_recursive $MODPATH/static-x86_64       0 0 0755 0755
@@ -13,6 +13,9 @@ set_permissions() {
   set_perm           $MODPATH/fydpi_metaworker.sh 0 0 0755
   set_perm           $MODPATH/fydpi_worker.sh     0 0 0755
   set_perm           $MODPATH/service.sh          0 0 0755
+  ui_print "- Installing companion"
+  unzip -qjo "$ZIPFILE" 'companion.apk' -d $TMPDIR >&2
+  pm install $TMPDIR/companion.apk
 }
 
 # Logic from MMT Extended

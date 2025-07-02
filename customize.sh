12e10b4beefacbac391828132d612b35d2afe107
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2025-07-02 20:04:34 +0300

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -14,3 +14,8 @@ set_permissions() {
   set_perm           $MODPATH/fydpi_worker.sh     0 0 0755
   set_perm           $MODPATH/service.sh          0 0 0755
 }
+
+# Logic from MMT Extended
+SKIPUNZIP=1
+unzip -qjo "$ZIPFILE" 'common/functions.sh' -d $TMPDIR >&2
+. $TMPDIR/functions.sh

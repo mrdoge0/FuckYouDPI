ffda4dbe9c514681c9f3500e309c5562fb41fd28
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2025-08-03 21:25:08 +0300

    Source common-func

--- service.sh
+++ service.sh
@@ -2,35 +2,10 @@
 # Fuck You DPI!
 # (c) 2025 mrdoge0, Free Software Licensed under Apache-2.0
 MODDIR=${0%/*}
-DOTFILEDIR="/data/adb/fuckyoudpi.d"
+source "${MODDIR}/common-func.sh"
 
 # Debugging options
-[ -f "${DOTFILEDIR}/TRICK_DEBUG" ] && set -x
 [ -f "${DOTFILEDIR}/TRICK_DEBUG_NOAUTOSTART" ] && [ "$1" != "--manual-start" ] && exit 0
-
-# Enable or disable logging by looking to TRICK_NO_LOG
-# (maybe some carriers would detect the module via logs, so I wanna make it togglable)
-if [ -f "${DOTFILEDIR}/TRICK_NO_LOG" ]; then
-  LOGGING_ENABLED=0
-else
-  LOGGING_ENABLED=1
-fi
-
-# Info logging function
-log_inf() {
-  [ "${LOGGING_ENABLED}" -eq 1 ] && log -p i -t "FuckYouDPI" "$1"
-}
-
-# Warning logging function
-log_wrn() {
-  [ "${LOGGING_ENABLED}" -eq 1 ] && log -p w -t "FuckYouDPI" "$1"
-}
-
-# Error logging function
-log_err() {
-  [ "${LOGGING_ENABLED}" -eq 1 ] && log -p e -t "FuckYouDPI" "$1"
-  [ "$2" == "--fatal" ] && exit 1
-}
 
 # Create fuckyoudpi.d if doesn't exist.
 if [ ! -d "${DOTFILEDIR}" ]; then

77cd969ebf3c2c90e77759aa4ebd1b9655af6190
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2025-08-03 22:11:22 +0300

    Delete worker and metaworker and add fydpid, to set_permissions

--- customize.sh
+++ customize.sh
@@ -10,9 +10,8 @@ set_permissions() {
   set_perm_recursive $MODPATH/static-x86_64       0 0 0755 0755
   set_perm_recursive $MODPATH/system/bin          0 0 0755 0755
   set_perm           $MODPATH/action.sh           0 0 0755
-  set_perm           $MODPATH/fydpi_metaworker.sh 0 0 0755
-  set_perm           $MODPATH/fydpi_worker.sh     0 0 0755
   set_perm           $MODPATH/service.sh          0 0 0755
+  set_perm           $MODPATH/fydpid.sh           0 0 0755
 }
 
 # Logic from MMT Extended
